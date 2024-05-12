a137d5ffbed389c56cfbdedaef8ba2090163e462
Abdul Wahab <abdul.wahab@codistan.org>
2024-05-12 23:38:36 +0500

    implemented jwt

--- Backend/Routes/v1/index.js
+++ Backend/Routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const jobseekerRoutes = require('./jobseekerRoutes');
+const authRoutes = require('./authRoutes');
 
 
 const router = express.Router();
 
+router.use('/auth', authRoutes);
 router.use('/jobseeker', jobseekerRoutes);
 
 
