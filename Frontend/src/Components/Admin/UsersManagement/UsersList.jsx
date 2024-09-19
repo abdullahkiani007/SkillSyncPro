320f0f980ad42e3da64ef1f2a3358a4a7bedb364
Hassan Mohiuddin <hassanmohiuddin73@gmail.com>
2024-09-20 04:42:22 +0500

    Admin  Files Updated

--- Frontend/src/Components/Admin/UsersManagement/UsersList.jsx
+++ Frontend/src/Components/Admin/UsersManagement/UsersList.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
 const UsersList = () => {
-  return <div>UsersList</div>;
-};
+  return <div>UsersList</div>
+}
 
-export default UsersList;
+export default UsersList
