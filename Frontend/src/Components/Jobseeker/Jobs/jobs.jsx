a0ddda4ef7db450fd9bf60d471d09dc0608546cf
Abdul Wahab <abdul.wahab@codistan.org>
2024-09-26 13:01:59 +0500

    fixed jobs recommendation

--- Frontend/src/Components/Jobseeker/Jobs/jobs.jsx
+++ Frontend/src/Components/Jobseeker/Jobs/jobs.jsx
@@ -1,208 +1,131 @@
 import React, { useState, useEffect } from "react";
 import JobSeekerController from "../../../API/jobseeker";
 import JobCard from "./JobCard";
+import { useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import JobFilter from "./JobFilter";
 
 function Jobs() {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [filteredJobs, setFilteredJobs] = useState([]);
-  const [filters, setFilters] = useState({
-    designer: "all",
-    location: "all",
-    experience: "all",
-    salary: "all",
-    salaryRange: 20000,
-  });
+  const [recommendedJobs, setRecommendedJobs] = useState([]);
+  const [showRecommended, setShowRecommended] = useState(false); // State for toggle
+  const [searchParams] = useSearchParams();
+
+  const userId = useSelector((store) => store.user)._id;
 
   useEffect(() => {
     const fetchJobs = async () => {
+      setLoading(true);
       try {
         const { data } = await JobSeekerController.getJobs();
-        console.log(data.jobs);
+        const formData = new FormData();
+        formData.append("user_id", userId);
+
+        const recommendedJobsResponse = await JobSeekerController.getRecommendedJbs(formData);
+        console.log("recommendedJobs: ", recommendedJobsResponse);
+
+        const recommendedJobIds = recommendedJobsResponse.recommended_jobs;
+
+        // Filter jobs based on recommended IDs
+        const filteredRecommendedJobs = data.jobs.filter((job) =>
+          recommendedJobIds.includes(job._id) // Assuming job._id matches the recommended job ID
+        );
+
         setJobs(data.jobs);
-        setFilteredJobs(data.jobs);
-        setLoading(false);
+        setRecommendedJobs(filteredRecommendedJobs);
         localStorage.setItem("jobs", JSON.stringify(data.jobs));
       } catch (error) {
         setError(error.message);
+      } finally {
         setLoading(false);
       }
     };
+
     fetchJobs();
-  }, []);
+  }, [userId]);
 
-  // useEffect(() => {
-  //   filterJobs();
-  // }, [filters, jobs]);
-
-  const handleFilterChange = (e) => {
-    const { id, value } = e.target;
-    console.log(id, value);
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      [id]: value,
-    }));
-    console.log("filtered ", filters);
-  };
+  useEffect(() => {
+    filterJobs();
+  }, [searchParams, jobs]);
 
   const filterJobs = () => {
     let filtered = [...jobs];
 
-    if (filters.designer !== "all") {
+    // Filtering by query params (as you have implemented)
+    const requirements = searchParams.get("requirements");
+    const company = searchParams.get("company");
+    const location = searchParams.get("location");
+    const salaryRange = searchParams.get("salaryRange");
+    const employmentType = searchParams.get("employmentType");
+    const applicants = searchParams.get("applicants");
+
+    if (requirements) {
       filtered = filtered.filter((job) =>
-        job.title.toLowerCase().includes(filters.designer)
+        job.requirements?.toLowerCase().includes(requirements?.toLowerCase())
       );
     }
 
-    if (filters.location !== "all") {
+    if (company) {
       filtered = filtered.filter((job) =>
-        job.location.toLowerCase().includes(filters.location)
+        job.company?.toLowerCase().includes(company?.toLowerCase())
       );
     }
 
-    if (filters.experience !== "all") {
+    if (location) {
       filtered = filtered.filter((job) =>
-        job.level.toLowerCase().includes(filters.experience)
+        job.location?.toLowerCase().includes(location?.toLowerCase())
       );
     }
 
-    if (filters.salary !== "all") {
-      const [minSalary, maxSalary] = filters.salary.split("-");
+    if (salaryRange) {
+      const [minSalary, maxSalary] = salaryRange.split("-");
       filtered = filtered.filter((job) => {
-        const salary = parseInt(job.salary.replace(/[^0-9]/g, ""));
+        const salary = parseInt(job.salaryRange.replace(/[^0-9]/g, ""));
         return salary >= minSalary && salary <= maxSalary;
       });
     }
 
-    if (filters.salaryRange) {
-      filtered = filtered.filter((job) => {
-        const salary = parseInt(job.salary.replace(/[^0-9]/g, ""));
-        return salary <= filters.salaryRange;
-      });
+    if (employmentType) {
+      filtered = filtered.filter((job) =>
+        job.employmentType?.toLowerCase().includes(employmentType?.toLowerCase())
+      );
     }
 
-    console.log("filtered ", filtered);
+    if (applicants) {
+      filtered = filtered.filter((job) => job.applicants.length <= parseInt(applicants));
+    }
+
     setFilteredJobs(filtered);
   };
 
   return (
     <div className="font-sans flex flex-col min-h-screen bg-gray-100">
       <div className="p-5">
-        <div className="flex space-x-5 mb-5">
-          <div className="flex flex-col space-y-2">
-            <label className="font-bold" htmlFor="designer">
-              Role
-            </label>
-            <select
-              id="designer"
-              className="p-2 border border-gray-300 rounded"
-              onChange={handleFilterChange}
-            >
-              <option value="all">All</option>
-              <option value="ui/ux">UI/UX</option>
-              <option value="graphic">Graphic</option>
-              <option value="motion">Motion</option>
-            </select>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <label className="font-bold" htmlFor="location">
-              Work location
-            </label>
-            <select
-              id="location"
-              className="p-2 border border-gray-300 rounded"
-              onChange={handleFilterChange}
-            >
-              <option value="all">All</option>
-              <option value="san-francisco">San Francisco</option>
-              <option value="new-york">New York</option>
-              <option value="california">California</option>
-            </select>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <label className="font-bold" htmlFor="experience">
-              Experience
-            </label>
-            <select
-              id="experience"
-              className="p-2 border border-gray-300 rounded"
-              onChange={handleFilterChange}
-            >
-              <option value="all">All</option>
-              <option value="junior">Junior</option>
-              <option value="middle">Middle</option>
-              <option value="senior">Senior</option>
-            </select>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <label className="font-bold" htmlFor="salary">
-              Per month
-            </label>
-            <select
-              id="salary"
-              className="p-2 border border-gray-300 rounded"
-              onChange={handleFilterChange}
-            >
-              <option value="all">All</option>
-              <option value="1200-2000">1,200 - 2,000</option>
-              <option value="2000-3000">2,000 - 3,000</option>
-              <option value="3000-4000">3,000 - 4,000</option>
-            </select>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <label className="font-bold" htmlFor="salaryRange">
-              Salary range
-            </label>
-            <input
-              type="range"
-              id="salaryRange"
-              min="1200"
-              max="20000"
-              className="p-2 border border-gray-300 rounded"
-              onChange={handleFilterChange}
-              value={filters.salaryRange}
-            />
-            <span>{filters.salaryRange}</span>
-          </div>
-        </div>
+        <JobFilter />
         <div className="p-5 rounded-lg shadow-md">
           <div className="flex justify-between items-center mb-5">
-            <div className="text-xl font-bold">Recommended jobs</div>
-            <div className="text-lg font-bold">{filteredJobs.length}</div>
+            <div className="text-xl font-bold">Jobs</div>
             <div className="flex space-x-2 items-center">
-              <label htmlFor="sort" className="font-semibold">
-                Sort by:
+              <label htmlFor="show-recommended" className="font-semibold">
+                Show Recommended Jobs Only:
               </label>
-              <select id="sort" className="p-2 border border-gray-300 rounded">
-                <option value="last-updated">Last updated</option>
-                <option value="salary">Salary</option>
-                <option value="date">Date</option>
-              </select>
+              <input
+                type="checkbox"
+                id="show-recommended"
+                checked={showRecommended}
+                onChange={() => setShowRecommended(!showRecommended)}
+              />
             </div>
+            <div className="text-lg font-bold">{(showRecommended ? recommendedJobs.length : filteredJobs.length) || 0}</div>
           </div>
           <div className="flex flex-wrap justify-around">
-            {filteredJobs.map((job) => (
+            {(showRecommended ? recommendedJobs : filteredJobs).map((job) => (
               <JobCard key={job._id} props={job} />
             ))}
           </div>
-        </div>
-        <div className="flex justify-center mt-5 space-x-5">
-          <button className="py-2 px-4 border rounded border-gray-300 text-gray-600">
-            1
-          </button>
-          <button className="py-2 px-4 border rounded border-gray-300 text-gray-600">
-            2
-          </button>
-          <button className="py-2 px-4 border rounded border-gray-300 text-gray-600">
-            3
-          </button>
-          <button className="py-2 px-4 border rounded border-gray-300 text-gray-600">
-            4
-          </button>
-          <button className="py-2 px-4 border rounded border-gray-300 text-gray-600">
-            ...
-          </button>
         </div>
       </div>
     </div>
