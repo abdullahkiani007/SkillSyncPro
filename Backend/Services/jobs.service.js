d88ded740718e93e2579a201aa7ea530371b21f4
Hassan Mohiuddin <hassanmohiuddin73@gmail.com>
2024-12-08 04:59:35 +0500

    Updated The Assessments Screens

--- Backend/Services/jobs.service.js
+++ Backend/Services/jobs.service.js
@@ -1,121 +1,123 @@
-const JobModel = require("../Models/job.model");
-const ApplicationModel = require("../Models/application.model");
-const JobseekerModel = require("../Models/jobseeker.model");
-const JobseekerService = require("../Services/jobSeeker.service");
-const UserModel = require("../Models/user.model");
-const EmployerModel = require("../Models/employer.model");
-const JobPerformance = require("../Models/jobPerfomance.model");
-const mongoose = require("mongoose");
+const JobModel = require('../Models/job.model')
+const ApplicationModel = require('../Models/application.model')
+const JobseekerModel = require('../Models/jobseeker.model')
+const JobseekerService = require('../Services/jobSeeker.service')
+const UserModel = require('../Models/user.model')
+const EmployerModel = require('../Models/employer.model')
+const JobPerformance = require('../Models/jobPerfomance.model')
+const mongoose = require('mongoose')
 
-const jobDTO = require("../DTO/jobsDTO");
-const jobseekerService = require("../Services/jobSeeker.service");
+const jobDTO = require('../DTO/jobsDTO')
+const jobseekerService = require('../Services/jobSeeker.service')
 const jobService = {
   getJobs: async () => {
     try {
-      const jobs = await JobModel.find().populate("company");
+      const jobs = await JobModel.find().populate('company')
+      console.log('jobs in service ', jobs)
       const newJobs = jobs.map((job) => {
-        return new jobDTO(job);
-      });
-      return newJobs;
+        return new jobDTO(job)
+      })
+      return newJobs
     } catch (error) {
-      return { status: 500, message: "Internal server error" };
+      return { status: 500, message: 'Internal server error' }
     }
   },
 
   // get all jobs for a jobseeker and add an applied field to each job
   getAllJobs: async (id) => {
     try {
-      console.log("Getting all the jobs", id);
-      const jobseeker = await JobseekerService.getJobSeeker(id);
-      console.log("Jobseeker , ", jobseeker);
-      const jobs = await JobModel.find().populate("company");
+      console.log('Getting all the jobs', id)
+      const jobseeker = await JobseekerService.getJobSeeker(id)
+      console.log('Jobseeker , ', jobseeker)
+      const jobs = await JobModel.find().populate('company')
+      console.log('jobs', jobs)
       const newJobs = jobs.map((job) => {
-        let applied = job.applicants.includes(jobseeker._id);
-        let jobDto = new jobDTO(job);
-        jobDto.applied = applied;
-        return jobDto;
-      });
-      return newJobs;
+        let applied = job.applicants.includes(jobseeker._id)
+        let jobDto = new jobDTO(job)
+        jobDto.applied = applied
+        return jobDto
+      })
+      return newJobs
     } catch (err) {
-      console.log(err);
-      return { status: 500, message: "Internal server error" };
+      console.log(err)
+      return { status: 500, message: 'Internal server error' }
     }
   },
 
   getJobDescription: async (id) => {
     try {
-      const job = await JobModel.findById(id);
+      const job = await JobModel.findById(id)
       return {
         description: job.description,
         requirements: job.requirements,
-      };
+      }
     } catch (err) {
-      return { status: 500, message: "Internal server error" };
+      return { status: 500, message: 'Internal server error' }
     }
   },
 
   getJob: async (id) => {
     try {
-      const job = await JobModel.findById(id).populate("company");
+      const job = await JobModel.findById(id).populate('company')
       return {
         job,
-      };
+      }
     } catch (error) {
-      return { status: 500, message: "Internal server error" };
+      return { status: 500, message: 'Internal server error' }
     }
   },
   addJob: async (jobId, userId, resume, coverLetter) => {
     try {
-      const jobseeker = await JobseekerService.getJobSeeker(userId);
+      const jobseeker = await JobseekerService.getJobSeeker(userId)
       const job = await JobModel.findByIdAndUpdate(
         jobId,
         {
           $push: { applicants: jobseeker._id },
         },
         { new: true }
-      );
+      )
 
       const newApplication = {
         job: jobId,
         jobSeeker: jobseeker._id,
         resume,
         coverLetter,
-      };
-      const application = await ApplicationModel.create(newApplication);
-      await jobseekerService.addApplication(jobseeker._id, application._id);
+      }
+      const application = await ApplicationModel.create(newApplication)
+      await jobseekerService.addApplication(jobseeker._id, application._id)
       return {
         status: 200,
         application,
-      };
+      }
     } catch (error) {
-      console.log(error);
+      console.log(error)
       return {
         status: 500,
-        message: "Internal Server Error",
-      };
+        message: 'Internal Server Error',
+      }
     }
   },
   getApplications: async (id) => {
     try {
-      const jobseekerId = await JobseekerService.getJobSeeker(id);
+      const jobseekerId = await JobseekerService.getJobSeeker(id)
       const jobseeker = await JobseekerModel.findById(jobseekerId._id).populate(
-        "applications"
-      );
+        'applications'
+      )
       return {
         status: 200,
         jobseeker,
-      };
+      }
     } catch (error) {
-      return { status: 500, message: "Internal server error" };
+      return { status: 500, message: 'Internal server error' }
     }
   },
 
   getAppliedJobs: async (id) => {
     try {
-      const jobseekerId = await JobseekerService.getJobSeeker(id);
+      const jobseekerId = await JobseekerService.getJobSeeker(id)
       const applications = await ApplicationModel.find({
         jobSeeker: jobseekerId._id,
-      }).populate("job");
+      }).populate('job')
 
       let jobs = applications.map((application) => {
         return {
@@ -131,88 +133,88 @@ const jobService = {
           videoIntroduction: application.videoIntroduction,
           feedback: application.feedback,
           rating: application.rating,
-        };
-      });
+        }
+      })
 
       jobs = jobs.filter((job) => {
-        return job.resume && job.videoIntroduction;
-      });
+        return job.resume && job.videoIntroduction
+      })
 
       return {
         status: 200,
         jobs,
-      };
+      }
     } catch (error) {
-      return { status: 500, message: "Internal server error" };
+      return { status: 500, message: 'Internal server error' }
     }
   },
 
   //
   archiveJobEmployer: async (jobId, userId) => {
     try {
-      const employer = await EmployerModel.findOne({ user: userId });
-      const job = await JobModel.findOne({ _id: jobId });
+      const employer = await EmployerModel.findOne({ user: userId })
+      const job = await JobModel.findOne({ _id: jobId })
 
       if (job.company.toString() !== employer.company.toString()) {
         return {
           status: 401,
-          message: "Unauthorized",
-        };
+          message: 'Unauthorized',
+        }
       }
       await JobModel.findByIdAndUpdate(jobId, {
         $set: { archived: !job.archived },
-      });
+      })
       return {
         status: 200,
-        message: "Job archived successfully",
-      };
+        message: 'Job archived successfully',
+      }
     } catch (err) {
-      console.log(err);
+      console.log(err)
       return {
         status: 500,
-        message: "Internal Server Error",
-      };
+        message: 'Internal Server Error',
+      }
     }
   },
 
   deleteJobEmployer: async (jobId, userId) => {
     try {
-      const employer = await EmployerModel.findOne({ user: userId });
-      const job = await JobModel.findOne({ _id: jobId });
+      const employer = await EmployerModel.findOne({ user: userId })
+      const job = await JobModel.findOne({ _id: jobId })
 
       if (job.company.toString() !== employer.company.toString()) {
         return {
           status: 401,
-          message: "Unauthorized",
-        };
+          message: 'Unauthorized',
+        }
       }
-      await JobModel.findByIdAndDelete(jobId);
+      await JobModel.findByIdAndDelete(jobId)
       return {
         status: 200,
-        message: "Job deleted successfully",
-      };
+        message: 'Job deleted successfully',
+      }
     } catch (err) {
-      console.log(err);
+      console.log(err)
       return {
         status: 500,
-        message: "Internal Server Error",
-      };
+        message: 'Internal Server Error',
+      }
     }
   },
 
   trackJobView: async (jobId, userId) => {
-    console.log("trackJobView");
-    console.log("Job viewed by user", userId);
+    console.log('trackJobView')
+    console.log('Job viewed by user', userId)
     try {
       // Set to today's date, midnight
-      const today = new Date();
-      today.setHours(0, 0, 0, 0);
+      const today = new Date()
+      today.setHours(0, 0, 0, 0)
 
       // Find or create a performance record for the job on today's date
       let jobPerformance = await JobPerformance.findOne({
         job: jobId,
         date: today,
-      });
+      })
 
       if (!jobPerformance) {
         // If no record exists, create a new one
@@ -221,54 +223,54 @@ const jobService = {
           date: today,
           views: 1,
           userViews: [userId],
-        });
-        console.log("Created new job performance record:", jobPerformance);
+        })
+        console.log('Created new job performance record:', jobPerformance)
       } else {
         // Update existing record
         if (!jobPerformance.userViews.includes(userId)) {
-          jobPerformance.userViews.push(userId);
-          jobPerformance.views += 1;
+          jobPerformance.userViews.push(userId)
+          jobPerformance.views += 1
           console.log(
-            "Updated existing job performance record:",
+            'Updated existing job performance record:',
             jobPerformance
-          );
+          )
         } else {
-          console.log("User has already viewed the job today.");
+          console.log('User has already viewed the job today.')
         }
       }
       // Save the updated performance record
-      await jobPerformance.save();
-      console.log("Saved job performance record.");
+      await jobPerformance.save()
+      console.log('Saved job performance record.')
     } catch (err) {
-      console.error("Error tracking job view:", err);
+      console.error('Error tracking job view:', err)
     }
   },
 
   trackJobApplication: async (jobId) => {
     try {
-      const today = new Date().setHours(0, 0, 0, 0);
+      const today = new Date().setHours(0, 0, 0, 0)
 
       await JobPerformance.findOneAndUpdate(
         { job: jobId, date: today },
         { $inc: { applications: 1 } },
         { upsert: true, new: true }
-      );
+      )
     } catch (err) {
-      console.error("Error tracking job application:", err);
+      console.error('Error tracking job application:', err)
     }
   },
 
   trackJobHire: async (jobId) => {
     try {
-      const today = new Date().setHours(0, 0, 0, 0);
+      const today = new Date().setHours(0, 0, 0, 0)
 
       await JobPerformance.findOneAndUpdate(
         { job: jobId, date: today },
         { $inc: { hires: 1 } },
         { upsert: true, new: true }
-      );
+      )
     } catch (err) {
-      console.error("Error tracking job hire:", err);
+      console.error('Error tracking job hire:', err)
     }
   },
 
@@ -277,24 +279,24 @@ const jobService = {
       return await JobPerformance.find({
         job: jobId,
         date: { $gte: startDate, $lte: endDate },
-      }).sort({ date: 1 });
+      }).sort({ date: 1 })
     } catch (err) {
-      console.error("Error retrieving job performance:", err);
+      console.error('Error retrieving job performance:', err)
     }
   },
 
   getJobPerformanceByDate: async (companyId, jobId, startDate, endDate) => {
-    console.log("In service function of getJobPerformanceByDate");
-    console.log("state", companyId, startDate, endDate, jobId);
+    console.log('In service function of getJobPerformanceByDate')
+    console.log('state', companyId, startDate, endDate, jobId)
 
     const matchCriteria = {
       date: { $gte: new Date(startDate), $lte: new Date(endDate) },
-      "job.company": new mongoose.Types.ObjectId(companyId),
-    };
+      'job.company': new mongoose.Types.ObjectId(companyId),
+    }
 
     // If a jobId is provided, add it to the match criteria
     if (jobId) {
-      matchCriteria["job._id"] = new mongoose.Types.ObjectId(jobId);
+      matchCriteria['job._id'] = new mongoose.Types.ObjectId(jobId)
     }
 
     const performances = await JobPerformance.aggregate([
@@ -305,14 +307,14 @@ const jobService = {
       },
       {
         $lookup: {
-          from: "jobs",
-          localField: "job",
-          foreignField: "_id",
-          as: "job",
+          from: 'jobs',
+          localField: 'job',
+          foreignField: '_id',
+          as: 'job',
         },
       },
       {
-        $unwind: "$job",
+        $unwind: '$job',
       },
       {
         $match: matchCriteria,
@@ -320,21 +322,21 @@ const jobService = {
       {
         $group: {
           _id: {
-            job_id: "$job._id",
-            date: "$date",
+            job_id: '$job._id',
+            date: '$date',
           },
-          views: { $sum: "$views" },
-          applications: { $sum: "$applications" },
-          hires: { $sum: "$hires" },
+          views: { $sum: '$views' },
+          applications: { $sum: '$applications' },
+          hires: { $sum: '$hires' },
         },
       },
       {
-        $sort: { "_id.date": 1 },
+        $sort: { '_id.date': 1 },
       },
-    ]);
+    ])
 
-    return performances;
+    return performances
   },
-};
+}
 
-module.exports = jobService;
+module.exports = jobService
