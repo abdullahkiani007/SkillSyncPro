08b8e0c05e22f71e2c4b1e6a905716ee31f51a4a
abdullahkiani007 <abdullahkiani931@gmail.com>
2024-12-08 15:48:11 +0500

    refactor(auth) : fixed auth header

--- Backend/Controllers/CompanyController.js
+++ Backend/Controllers/CompanyController.js
@@ -1,261 +1,256 @@
-const companyService = require('../Services/company.service');
+const companyService = require("../Services/company.service");
 
 const CompanyController = {
-    async register(req,res,next){
-        const {_id} = req.user;
+  async register(req, res, next) {
+    const { _id } = req.user;
 
-        const company = {
-            ...req.body,
-            createdBy:_id
-        }
+    const company = {
+      ...req.body,
+      createdBy: _id,
+    };
 
-        const nameExists = await companyService.nameExists(company.name);
-        if (nameExists){
-            res.status(403).json({
-                "message":"Name already exists"
-            })
-            return ;
-        }
-        const response = await companyService.register(company);
-        if (response.status == 200){
-            res.status(200).json({
-                "message":"Company registered successfully",
-                "data":response.data
-            })
-            return ;
-        }
-
-        res.status(500).json({
-            "message":"Internal server error",
-        })
-
-    },
-
-    
-
-    
-
-    async getCompany(req,res,next){
-        console.log("GET company received");
-        const {_id} = req.user;
-
-        const response = await companyService.getCompanybyUserId(_id);
-        if (response.status == 200){
-            
-            res.status(200).json({
-                "data":response.data
-            })
-            return ;
-        }else if (response.status == 404){
-            res.status(404).json({
-                "message":response.message
-            })
-            return ;
-        }
-
-        res.status(404).json({
-            "message":"Company not found"
-        })
-    },
-    async getCompanyNames(req,res,next){
-        console.log("GET company names received");
-        try{
-            const companies = await companyService.getCompanyNames();
-            res.status(200).json({
-                companies
-            })
-        }catch(err){
-            console.log(err)
-            next(err)
-        }
+    const nameExists = await companyService.nameExists(company.name);
+    if (nameExists) {
+      res.status(403).json({
+        message: "Name already exists",
+      });
+      return;
     }
-    ,
-    async updateCompany(req,res,next){
-        console.log("PUT company received");
-        const {_id} = req.user;
-
-        const company = {
-            ...req.body,
-            createdBy:_id
-        }
-
-        const response = await companyService.updateCompany(company);
-        if (response.status == 200){
-            res.status(200).json({
-                "message":"Company updated successfully",
-                "data":response.data
-            })
-            return ;
-        }else if (response.status == 404){
-            res.status(404).json({
-                "message":response.message
-            })
-            return ;
-        }
-
-        res.status(500).json({
-            "message":"Internal server error"
-        })
-    },
-
-    async getEmployees(req,res,next){
-        console.log("Get/ employees received")
-      const {id} = req.user;
-      const {companyId} = req.query;
-
-      try{
-
-      
-        const employees = await companyService.getEmployees(companyId);
-            res.status(200).json({
-                employees
-            })
-            return ;
-      }catch(err){
-        console.log(err)
-            next(err)
-      }
-        
-
-    }, async joinCompany(req,res,next){
-        const {id} = req.user;
-        const companyId = req.query.id;
-        const response = await companyService.joinCompany(id,companyId);
-        if (response.status == 200){
-            res.status(200).json({
-                "message":"Joined company successfully",
-                "data":response.data
-            })
-            return ;
-        }else if (response.status == 404){
-            res.status(404).json({
-                "message":response.message
-            })
-            return ;
-        }else if (response.status == 403){
-            res.status(403).json({
-                "message":response.message
-            })
-            return ;
-        }
-
-        res.status(500).json({
-            "message":"Internal server error"
-        })
-    },
-
-    async leaveCompany(req,res,next){
-        const {id} = req.user;
-        const response = await companyService.leaveCompany(id);
-        if (response.status == 200){
-            res.status(200).json({
-                "message":"Left company successfully",
-                "data":response.data
-            })
-            return ;
-        }else if (response.status == 404){
-            res.status(404).json({
-                "message":response.message
-            })
-            return ;
-        }else if (response.status == 403){
-            res.status(403).json({
-                "message":response.message
-            })
-            return ;
-        }
-
-        res.status(500).json({
-            "message":"Internal server error"
-        })
-
+    const response = await companyService.register(company);
+    if (response.status == 200) {
+      res.status(200).json({
+        message: "Company registered successfully",
+        data: response.data,
+      });
+      return;
     }
 
-    ,
-    async authorizeEmployee(req,res,next){
-        const {employeeId , companyId} = req.body;
-        console.log("Authorize employee received")
-        console.log(employeeId,companyId)
-        const {id} = req.user;
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  },
 
-        try{
-            const response = await companyService.authorizeEmployee(employeeId,id,companyId);
-            console.log(response)
-           return res.status(response.status).json({
-                "message":response.message
-            })
-        }catch(err){
-            console.log(err)
-            next(err)
-        }
+  async getCompany(req, res, next) {
+    console.log("GET company received");
+    const { _id } = req.user;
 
-
-    },
-    async revokeEmployee(req,res,next){
-        const {employeeId , companyId} = req.body;
-        console.log("Revoke employee received")
-        console.log(employeeId,companyId)
-        const {id} = req.user;
-
-        try{
-            const response = await companyService.revokeEmployee(employeeId,id,companyId);
-           return res.status(response.status).json({
-                "message":response.message
-            })
-        }catch(err){
-            console.log(err)
-            next(err)
-        }
-    },
-    async deleteJoinRequest(req,res,next){
-        const {employeeId , companyId} = req.body;
-        console.log("Delete join request received")
-        console.log(employeeId,companyId)
-        const {id} = req.user;
-
-        try{
-            const response = await companyService.deleteJoinRequest(employeeId,id,companyId);
-           return res.status(response.status).json({
-                "message":response.message
-            })
-        }catch(err){
-            console.log(err)
-            next(err)
-        }
+    const response = await companyService.getCompanybyUserId(_id);
+    if (response.status == 200) {
+      res.status(200).json({
+        data: response.data,
+      });
+      return;
+    } else if (response.status == 404) {
+      res.status(404).json({
+        message: response.message,
+      });
+      return;
     }
-    ,
-    async getCompanies(req,res,next){
-        console.log("GET/ Companies received")
-        const response = await companyService.getCompanies();
-        if (response.status === 200){
-            return (res.status(200).json({
-                "data":response.companies
-            }))
-        }else{
-            return (res.status(500).json({
-                "message":"Internal Server Error"
-            }))
-        }
-    },
 
-    async getAssessment(req,res,next){
-        console.log("GET/ Assessment received")
-        const companyId = req.query.companyId;
-        const response = await companyService.getAssessment(companyId);
-        if (response.status === 200){
-            return (res.status(200).json({
-                "assessment":response.assessment
-            }))
-        }
-        else if (response.status === 404){
-            return (res.status(404).json({
-                "message":response.message
-            }))
-        }
-        return (res.status(500).json({
-            "message":"Internal Server Error"
-        }))
+    res.status(404).json({
+      message: "Company not found",
+    });
+  },
+  async getCompanyNames(req, res, next) {
+    console.log("GET company names received");
+    try {
+      const companies = await companyService.getCompanyNames();
+      res.status(200).json({
+        companies,
+      });
+    } catch (err) {
+      console.log(err);
+      next(err);
     }
-}
+  },
+  async updateCompany(req, res, next) {
+    console.log("PUT company received");
+    const { _id } = req.user;
 
-module.exports  = CompanyController;
+    const company = {
+      ...req.body,
+      createdBy: _id,
+    };
+
+    const response = await companyService.updateCompany(company);
+    if (response.status == 200) {
+      res.status(200).json({
+        message: "Company updated successfully",
+        data: response.data,
+      });
+      return;
+    } else if (response.status == 404) {
+      res.status(404).json({
+        message: response.message,
+      });
+      return;
+    }
+
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  },
+
+  async getEmployees(req, res, next) {
+    console.log("Get/ employees received");
+    const { id } = req.user;
+    const { companyId } = req.query;
+
+    try {
+      const employees = await companyService.getEmployees(companyId);
+      res.status(200).json({
+        employees,
+      });
+      return;
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  },
+  async joinCompany(req, res, next) {
+    const { id } = req.user;
+    const companyId = req.query.id;
+    const response = await companyService.joinCompany(id, companyId);
+    if (response.status == 200) {
+      res.status(200).json({
+        message: "Joined company successfully",
+        data: response.data,
+      });
+      return;
+    } else if (response.status == 404) {
+      res.status(404).json({
+        message: response.message,
+      });
+      return;
+    } else if (response.status == 403) {
+      res.status(403).json({
+        message: response.message,
+      });
+      return;
+    }
+
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  },
+
+  async leaveCompany(req, res, next) {
+    const { id } = req.user;
+    const response = await companyService.leaveCompany(id);
+    if (response.status == 200) {
+      res.status(200).json({
+        message: "Left company successfully",
+        data: response.data,
+      });
+      return;
+    } else if (response.status == 404) {
+      res.status(404).json({
+        message: response.message,
+      });
+      return;
+    } else if (response.status == 403) {
+      res.status(403).json({
+        message: response.message,
+      });
+      return;
+    }
+
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  },
+
+  async authorizeEmployee(req, res, next) {
+    const { employeeId, companyId } = req.body;
+    console.log("Authorize employee received");
+    console.log(employeeId, companyId);
+    const { id } = req.user;
+
+    try {
+      const response = await companyService.authorizeEmployee(
+        employeeId,
+        id,
+        companyId
+      );
+      return res.status(response.status).json({
+        message: response.message,
+      });
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  },
+  async revokeEmployee(req, res, next) {
+    const { employeeId, companyId } = req.body;
+    console.log("Revoke employee received");
+    console.log(employeeId, companyId);
+    const { id } = req.user;
+
+    try {
+      const response = await companyService.revokeEmployee(
+        employeeId,
+        id,
+        companyId
+      );
+      return res.status(response.status).json({
+        message: response.message,
+      });
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  },
+  async deleteJoinRequest(req, res, next) {
+    const { employeeId, companyId } = req.body;
+    console.log("Delete join request received");
+    console.log(employeeId, companyId);
+    const { id } = req.user;
+
+    try {
+      const response = await companyService.deleteJoinRequest(
+        employeeId,
+        id,
+        companyId
+      );
+      return res.status(response.status).json({
+        message: response.message,
+      });
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  },
+  async getCompanies(req, res, next) {
+    console.log("GET/ Companies received");
+    const response = await companyService.getCompanies();
+    if (response.status === 200) {
+      return res.status(200).json({
+        data: response.companies,
+      });
+    } else {
+      return res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+  },
+
+  async getAssessment(req, res, next) {
+    console.log("GET/ Assessment received");
+    const companyId = req.query.companyId;
+    const response = await companyService.getAssessment(companyId);
+    if (response.status === 200) {
+      return res.status(200).json({
+        assessment: response.assessment,
+      });
+    } else if (response.status === 404) {
+      return res.status(404).json({
+        message: response.message,
+      });
+    }
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  },
+};
+
+module.exports = CompanyController;
--- Frontend/src/Components/Employer/Chatbot/MessageParser.js
+++ Frontend/src/Components/Employer/Chatbot/MessageParser.js
@@ -1,24 +1,23 @@
 class MessageParser {
-    constructor(actionProvider) {
-      this.actionProvider = actionProvider;
-    }
-  
-    parse(message) {
-      const lowerCaseMessage = message.toLowerCase();
-  
-      if (lowerCaseMessage.includes("candidate skills")) {
-        this.actionProvider.handleCandidateSkills();
-      } else if (lowerCaseMessage.includes("education")) {
-        this.actionProvider.handleCandidateEducation();
-      } else if (lowerCaseMessage.includes("schedule interview")) {
-        this.actionProvider.handleScheduleInterview();
-      } else if (lowerCaseMessage.includes("applicant metrics")) {
-        this.actionProvider.handleApplicantMetrics();
-      } else {
-        this.actionProvider.handleDefaultResponse();
-      }
+  constructor(actionProvider) {
+    this.actionProvider = actionProvider;
+  }
+
+  parse(message) {
+    const lowerCaseMessage = message.toLowerCase();
+
+    if (lowerCaseMessage.includes("candidate skills")) {
+      this.actionProvider.handleCandidateSkills();
+    } else if (lowerCaseMessage.includes("education")) {
+      this.actionProvider.handleCandidateEducation();
+    } else if (lowerCaseMessage.includes("schedule interview")) {
+      this.actionProvider.handleScheduleInterview();
+    } else if (lowerCaseMessage.includes("applicant metrics")) {
+      this.actionProvider.handleApplicantMetrics();
+    } else {
+      this.actionProvider.handleDefaultResponse(lowerCaseMessage);
     }
   }
-  
-  export default MessageParser;
-  
+}
+
+export default MessageParser;
